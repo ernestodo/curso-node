13e069ddaedfa8ea64ff25373559ce6a8693c128
Ernesto Domínguez <ernesto.dmg@gmail.com>
2023-08-30 21:20:44 -0600

    first commit

---
+++ bases-node/app.js
@@ -0,0 +1,12 @@
+console.clear();
+console.log('==============');
+console.log('Tabla del 5');
+console.log('==============');
+const base = 5;
+
+for (let index = 1; index <= 10; index++){
+    console.log(`${base} x ${index} = ${base*index}`);   
+}
+
+
+hola
